de502db7250683e3f44317751cbe1c46e80d8e6a
jatinlalwani97 <46535630+jatinlalwani97@users.noreply.github.com>
2019-04-13 15:47:01 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+app.post('/search', (req, res) => {
+  //res.send('You sent the name "' + req.body.name + '".');
+  res.redirect('https://api.github.com/users/' + req.body.name);
+});
+
+app.listen(port,() => {
+  console.log(`Server is up on port ${port}`);
+});
